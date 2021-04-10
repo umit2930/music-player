8902e922adfa940c7ed2782fe291b90fd0497fd6
kaveh <20kaveh1999@gmail.com>
2021-04-10 02:49:20 -0700

    add timer to splash

--- app/src/main/java/com/example/music/view/SplashFragment.java
+++ app/src/main/java/com/example/music/view/SplashFragment.java
@@ -1,6 +1,7 @@
 package com.example.music.view;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,19 +9,34 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.example.music.R;
 import com.example.music.databinding.FragmentSplashBinding;
 
 public class SplashFragment extends Fragment {
 
-
-
     FragmentSplashBinding binding;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding=FragmentSplashBinding.inflate(getLayoutInflater());
         return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+        new CountDownTimer(3000, 1000) {
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                Navigation.findNavController(binding.getRoot()).navigate(R.id.action_splashFragment_to_homeFragment);
+            }
+        }.start();
     }
 }
